b1798e552b5375aeec5fedd49a248ac8806c5fc6
beargh <32588026+xiong96gh@users.noreply.github.com>
2020-10-20 13:33:22 +0800

    Update Debian_Python3.6.sh

--- yijian/python3.6/Debian_Python3.6.sh
+++ yijian/python3.6/Debian_Python3.6.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt-get update -y
 apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
-wget https://raw.githubusercontent.com/xiong96gh/pic_bed/master/yijian/Python-3.6.4.tar.gz && tar zxvf Python-3.6.4.tar.gz && cd Python-3.6.4
+wget https://raw.githubusercontent.com/xiong96gh/pic_bed/master/yijian/python3.6/Python-3.6.4.tar.gz && tar zxvf Python-3.6.4.tar.gz && cd Python-3.6.4
 ./configure
 make&& make install
 cd ..
